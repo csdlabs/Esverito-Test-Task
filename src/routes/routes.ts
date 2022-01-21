c62a4091389d51ed4112f0af51a0a7f2736c3abe
baldezzzzh <pidenkodmitriy@gmail.com>
2022-01-21 16:03:24 +0400

    fix route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,6 @@
 export enum ROUTES {
     HOME = '/',
-    HOME_GIT = '/Esverito-Test-Task/',
+    HOME_GIT = '/Esverito-Test-Task',
     LOGIN = '/login',
     CAR_LIST = '/car-list',
     CAR = '/car',
